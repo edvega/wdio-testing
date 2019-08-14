161a09ceeb3b61265ec5069f3e8d1267613c8015
edvega <edwega@gmail.com>
2019-08-14 16:57:58 -0500

    POM, updated Login test

--- tests/login.test.js
+++ tests/login.test.js
@@ -1,36 +1,17 @@
 import { expect } from "chai";
 import * as dataHelper from "../lib/data-helpers";
+import LoginPage from "../page-objects/LoginPage";
 
 describe("Login test", () => {
 
-    it("Should navigate to homepage", () => {
-        const url = "http://zero.webappsecurity.com/index.html";
-        browser.url(url);
-
-        const metadata = browser.getMetadata();
-        expect(metadata.title).to.contains("Personal Banking");
-        expect(metadata.url).to.contains("index.html");
-    });
-
-    it("Should click on signin button", () => {
-        browser.waitAndClick("#signin_button");
-        
-        const metadata = browser.getMetadata();
-        expect(metadata.url).to.contains("login.html");
-    });
-
-    it("Should submit login form", () => {
-        const loginForm = $("#login_form");
-
-        loginForm.waitForExist(5000);
-        browser.waitAndTypeText("#user_login", dataHelper.getRandomName());
-        browser.waitAndTypeText("#user_password", "random password");
-        browser.waitAndClick(".btn-primary");
-    });
-
-    it("Should show error message", () => {
-        const errorMessage = $(".alert-error");
-        expect(errorMessage.getText()).to.contains(
+    it("Should deny access with wrong creds", () => {
+        LoginPage.open();
+        LoginPage.loginForm.waitForExist();
+        LoginPage.username.setValue("foo");
+        LoginPage.password.setValue("bar");
+        LoginPage.submitButton.click();
+        LoginPage.wait(2000);
+        expect(LoginPage.errorMessage.getText()).to.contains(
             "Login and/or password are wrong");
     });
 });
