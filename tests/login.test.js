e01bea9811a6757f9f5880034375910f809093c5
edvega <edwega@gmail.com>
2019-08-15 16:57:08 -0500

    Full E2E test, login/logout flow

--- tests/login.test.js
+++ tests/login.test.js
@@ -1,6 +1,7 @@
 import { expect } from "chai";
 import * as dataHelper from "../lib/data-helpers";
 import LoginPage from "../page-objects/LoginPage";
+import { username, password } from "../lib/config";
 
 describe("Login test", () => {
 
@@ -9,5 +10,18 @@ describe("Login test", () => {
         LoginPage.loginToApplication("foo", "bar");
         expect(LoginPage.errorMessage.getText()).to.contains(
             "Login and/or password are wrong");
+    });
+
+    it("Should login to application", () => {
+        LoginPage.open();
+        LoginPage.loginToApplication(username, password);
+    });
+
+    it("Should logout from application", () => {
+        $(".icon-user").waitForExist();
+        $(".icon-user").click();
+        $("#logout_link").waitForExist();
+        $("#logout_link").click();
+        $("#logout_link").waitForExist(5000, true);
     });
 });
